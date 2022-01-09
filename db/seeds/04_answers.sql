903f63981cb75d561e70f4ab94c44f6f025dc611
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-08 20:37:51 -0500

    more seeds for answers table

--- db/seeds/04_answers.sql
+++ db/seeds/04_answers.sql
@@ -1,5 +1,20 @@
 INSERT INTO answers (question_id, text, correct) VALUES (1, 'soccer', true),
+(1, 'Basketball', false),
+(1, 'Baseball', false),
+(1, 'Hockey', false),
 (2, '26', true),
+(2, '32', false),
+(2, '12', false),
+(2, '84', flase),
 (3, 'bh/2', true),
+(3, 'bh', false),
+(3, 'bh/4', false),
+(3, '3bh/2', false),
 (4, 'Ottawa', true),
+(4, 'Toronto', false),
+(4, 'Montreal', false),
+(4, 'Vancouver', false),
 (5, 'Dwayne Johnson', true);
+(5, 'Anthony Hopkins', false);
+(5, 'Riz Ahmed', false);
+(5, 'Steven Yeun', false);
