866fbf449cf698de1d46621ae438c92bbc1091ce
Adeyemi <henry.atayero@gmail.com>
2022-01-08 21:18:02 -0600

    updated question_id to quiz_id on user_quizzes schema

--- db/schema/05_users_quizzes.sql
+++ db/schema/05_users_quizzes.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS users_quizzes CASCADE;
 CREATE TABLE users_quizzes (
-  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   PRIMARY KEY (question_id, user_id),
   correct INTEGER NOT NULL,
