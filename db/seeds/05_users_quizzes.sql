8e0fe0a29de9206e4c3925b4720fd2aeca60c275
Adeyemi <henry.atayero@gmail.com>
2022-01-08 21:18:54 -0600

    updated question_id to quiz_id on user_quizzes seeds file

--- db/seeds/05_users_quizzes.sql
+++ db/seeds/05_users_quizzes.sql
@@ -1,4 +1,4 @@
-INSERT INTO users_quizzes (question_id, user_id, correct, url) VALUES (1,1,1,'http://site1.com'),
+INSERT INTO users_quizzes (quiz_id, user_id, correct, url) VALUES (1,1,1,'http://site1.com'),
 (2,2,0,'http://site2.com'),
 (3,3,1,'http://site3.com'),
 (4,4,1,'http://site4.com'),
