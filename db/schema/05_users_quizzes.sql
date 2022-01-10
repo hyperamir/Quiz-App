6e7e43313a0b154da2074166871cf56c6cb2a37d
Adeyemi <henry.atayero@gmail.com>
2022-01-10 11:59:58 -0600

    creating the schemas

--- db/schema/05_users_quizzes.sql
+++ db/schema/05_users_quizzes.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS users_quizzes CASCADE;
 CREATE TABLE users_quizzes (
   quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  PRIMARY KEY (question_id, user_id),
+  PRIMARY KEY (quiz_id, user_id),
   correct INTEGER NOT NULL,
   url VARCHAR(255) NOT NULL
 );
