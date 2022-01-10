6683e27cfa334a5800f965c89c1d3ce83791857a
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-10 09:41:15 -0500

    fix minor typo issues

--- db/seeds/04_answers.sql
+++ db/seeds/04_answers.sql
@@ -5,7 +5,7 @@ INSERT INTO answers (question_id, text, correct) VALUES (1, 'soccer', true),
 (2, '26', true),
 (2, '32', false),
 (2, '12', false),
-(2, '84', flase),
+(2, '84', false),
 (3, 'bh/2', true),
 (3, 'bh', false),
 (3, 'bh/4', false),
@@ -14,7 +14,7 @@ INSERT INTO answers (question_id, text, correct) VALUES (1, 'soccer', true),
 (4, 'Toronto', false),
 (4, 'Montreal', false),
 (4, 'Vancouver', false),
-(5, 'Dwayne Johnson', true);
-(5, 'Anthony Hopkins', false);
-(5, 'Riz Ahmed', false);
+(5, 'Dwayne Johnson', true),
+(5, 'Anthony Hopkins', false),
+(5, 'Riz Ahmed', false),
 (5, 'Steven Yeun', false);
