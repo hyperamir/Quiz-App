d9c992b9bc8b85b3541f44b8860ae695bce09137
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-01-11 11:50:05 -0500

    add POST to /create (hardcoded)

--- routes/create.js
+++ routes/create.js
@@ -6,11 +6,12 @@ module.exports = (db) => {
     res.render('create.ejs');
   });
 
-  router.post('/:id', (req, res) => {
+  router.post('/', (req, res) => {
 
-    const userId = req.session.id
-    console.log(req.body)
-    const quiz = req.body.quiz;
+    const userId = req.session.user_id;
+    console.log(userId);
+    console.log(req.body);
+    const name = req.body.quiz;
     const category = req.body.category;
     const question = req.body.question;
     const correct = req.body.correct;
@@ -20,8 +21,51 @@ module.exports = (db) => {
 
     const query = `
       INSERT INTO quizzes (user_id, name, created_at,url, category, listed)
-      VALUES ()
+      VALUES ($1, $2, $3, $4, $5, $6);
     `
+    const values = [userId, name, 'now()', 'http://localhost:8080/quiz/beq9n',category, 'true' ]
+
+    const questionQuery = `
+      INSERT INTO questions (quiz_id, text)
+      VALUES ($1, $2);
+    `
+    const questionValues = [5, question];
+
+    const answerAQuery = `
+      INSERT INTO answers (question_id, text, correct)
+      VALUES ($1, $2, $3);
+    `
+    const answerAValues = [6, correct, 'true'];
+
+    const answerBQuery = `
+      INSERT INTO answers (question_id, text, correct)
+      VALUES ($1, $2, $3);
+    `
+    const answerBValues = [6, answerB, 'false'];
+    const answerCQuery = `
+      INSERT INTO answers (question_id, text, correct)
+      VALUES ($1, $2, $3);
+    `
+    const answerCValues = [6, answerC, 'false'];
+    const answerDQuery = `
+      INSERT INTO answers (question_id, text, correct)
+      VALUES ($1, $2, $3);
+    `
+    const answerDValues = [6, answerD, 'false'];
+
+
+
+
+    db.query(questionQuery, questionValues);
+    db.query(answerAQuery, answerAValues);
+    db.query(answerBQuery, answerBValues);
+    db.query(answerCQuery, answerCValues);
+    db.query(answerDQuery, answerDValues);
+
+    db.query(query, values)
+    .then(()=>{
+      res.redirect('/users');
+    })
   })
 
   return router;
