eb1c4334d00285e07dc5f64fd87acffd32693719
Adeyemi <henry.atayero@gmail.com>
2022-01-11 12:11:27 -0600

    updating new routes for each quiz and their ejs files

---
+++ routes/fourthquiz.js
@@ -0,0 +1,33 @@
+/*
+ * All routes for Widgets are defined here
+ * Since this file is loaded in server.js into api/widgets,
+ *   these routes are mounted onto /widgets
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT questions.text AS question, answers.text as answer
+    FROM questions
+    JOIN answers ON questions.id = question_id
+    WHERE questions.id BETWEEN $1 AND $2` , [15, 19])
+      .then(data => {
+        console.log(data.rows)
+        results = data.rows;  //
+        templateVars = { results } //
+        res.render('fourthquiz.ejs', templateVars); //
+        //res.json(data.rows[0]);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      })
+  })
+
+  return router;
+};
+
