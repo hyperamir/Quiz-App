22a2537adba98c0ed606bd7be16abb47132ab191
Aisha Sabrie <sabriealt@gmail.com>
2022-01-11 17:43:23 -0500

    Make Now(); the default for created_at

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -6,7 +6,7 @@ CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP,
+  created_at TIMESTAMP DEFAULT Now();,
   url VARCHAR (255),
   category VARCHAR(255),
   listed BOOLEAN DEFAULT TRUE
