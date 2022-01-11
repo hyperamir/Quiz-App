21cbb3c12019fd4f133a9d75fe34acdf46f90419
Aisha Sabrie <sabriealt@gmail.com>
2022-01-11 02:26:19 -0500

    table schemas and created more robust seed data

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,6 +1,7 @@
 -- Widgets table seeds here (Example)
-INSERT INTO quizzes (user_id, name, created_at,url, category, listed) VALUES (2,'quiz01', now(), 'http://exmple1.com', 'sport',true),
-(4,'quiz02', now(), 'http://exmple2.com', 'math',true),
-(3,'quiz03', now(), 'http://exmple3.com', 'math',true),
-(1,'quiz04', now(), 'http://exmple4.com', 'geography',true),
-(5,'quiz05', now(), 'http://exmple5.com', 'art',true);
+INSERT INTO quizzes (user_id, name, created_at,url, category, listed)
+VALUES (2,'How well do you know sports?', now(), 'http://localhost:8080/quiz/ji1bn', 'Sports',true),
+(4,'Do you actually remember BEDMAS?', now(), 'http://localhost:8080/quiz/0pyg9', 'Math',true),
+(3,'Finish the Vine qoutes', now(), 'http://localhost:8080/quiz/rgj31', 'Pop Culture',true),
+(1,'World Capitals', now(), 'http://localhost:8080/quiz/1djpe', 'Geography',true),
+(5,'Which is not like the rest?', now(), 'http://localhost:8080/quiz/q7qjd', 'Trivia',true);
--- db/seeds/03_questions.sql
+++ db/seeds/03_questions.sql
@@ -1,7 +1,24 @@
-INSERT INTO questions (quiz_id, text) VALUES (1, 'What sport has the most fans in the world?'),
-(1, 'What sport has the most fans in the europe?'),
-(1, 'What sport has the most fans in the asia?'),
-(2, 'Whats the answer of 4 * 6 + 8 / 4 ?'),
-(3, 'Formula for calculation area of triangle?'),
-(4, 'Captial of Canada?'),
-(5, 'Best actor in the world at 2020?');
+INSERT INTO questions (quiz_id, text)
+VALUES (1, 'What sport has the most fans in the world?'),
+(1, 'What sport was not in the 2020 olympics?'),
+(1, 'What nba player has the most rings?'),
+(1, 'Which of the following countries has not hoseted the olympics?'),
+(1, 'Eulid Kipchoge is the only person to run a sub 2h marathon. Which country does he race for?'),
+(1, 'What is the championship trophy for the CFL called?'),
+(2, 'Which of the following pairs of operations are in correct order?'),
+(2, 'Whats the answer of 4 x 6 + 8 ÷ 4 ?'),
+(2, 'Which of the following equations has a larger outcome? 7 x 3 + 2 or 7 x ( 3 + 2)'),
+(2, 'What is the answer of (8/(2²)) x 1/2'),
+(3, 'Stop! I couldda dropped my ...'),
+(3,'Road work ahead?...')
+(3, "Sleep? I don't know about sleep ..." )
+(3,'When will you learn...' )
+(4, 'What is the captial of Canada?'),
+(4, 'What is the captial of Colombia?'),
+(4, 'What is the captial of the country with the largest population?'),
+(4, 'What is the captial of the country with the largest area?'),
+(4, 'What is the captial of the first country to elect a female president?'),
+(5, 'Which of the following are not shades of red?'),
+(5, 'Which country does not have Spanish as an official language?'),
+(5, 'Which of these birds can fly?'),
+(5, "Which countries capital does not have the same name as it's country?");
