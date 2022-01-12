62cd566ec4a9edfd72e8e093f0bd450a8b1aa9bf
Adeyemi <henry.atayero@gmail.com>
2022-01-12 16:33:51 -0600

    making quiz dynamic

--- routes/fifthquiz.js
+++ routes/fifthquiz.js
@@ -7,39 +7,45 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 module.exports = (db) => {
   router.get("/:id", (req, res) => {
     const quiz_id = req.params.id;
 
-    const firstQuestion = db.query(`SELECT questions.id FROM questions WHERE quiz_id = $1 LIMIT 1;`, [quiz_id])
+    db.query(`SELECT questions.id FROM questions WHERE quiz_id = $1 LIMIT 1;`, [quiz_id])
       .then((data) => {
-        return data.rows[0];
-      });
-    const lastQuestion = db.query(`SELECT questions.id FROM questions WHERE quiz_id = $1 ORDER BY questions.id DESC LIMIT 1;`, [quiz_id])
-      .then((data) => {
-        return data.rows[0];
-      });
+        //return data.rows[0];
+        const firstQuestion = data.rows[0].id;
 
-    console.log(firstQuestion);
-    console.log(lastQuestion);
-    db.query(`SELECT questions.text AS question, answers.text as answer
+        db.query(`SELECT questions.id FROM questions WHERE quiz_id = $1 ORDER BY questions.id DESC LIMIT 1;`, [quiz_id])
+          .then((data) => {
+            // return data.rows[0];
+            const lastQuestion = data.rows[0].id;
+            console.log(firstQuestion, lastQuestion);
+
+            console.log(lastQuestion);
+            db.query(`SELECT questions.text AS question, answers.text as answer
     FROM questions
     JOIN answers ON questions.id = question_id
-    WHERE questions.id BETWEEN $1 AND $2` , [firstQuestion,lastQuestion])
-      .then(data => {
-        console.log(data.rows);
-        const results = data.rows;  //
-        const templateVars = { results };//
-        res.render('fifthquiz.ejs', templateVars); //
-        //res.json(data.rows[0]);
+    WHERE questions.id BETWEEN $1 AND $2` , [firstQuestion, lastQuestion])
+              .then(data => {
+                console.log(data.rows);
+                const results = data.rows;  //
+                const templateVars = { results };//
+                res.render('fifthquiz.ejs', templateVars); //
+                //res.json(data.rows[0]);
+              })
+          });
+
+
       })
       .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
       });
+
   });
 
   return router;
