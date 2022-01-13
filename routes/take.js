0e158b2c022e2653697f03b6bfbee40f43bdab58
Adeyemi <henry.atayero@gmail.com>
2022-01-12 21:17:06 -0600

    updating the take.js route

--- routes/take.js
+++ routes/take.js
@@ -12,6 +12,7 @@ const router = express.Router();
 module.exports = (db) => {
   router.get("/:id", (req, res) => {
     const quiz_id = req.params.id;
+    //req.session.params_id = ;
 
     db.query(`SELECT questions.id FROM questions WHERE quiz_id = $1 LIMIT 1;`, [quiz_id])
       .then((data) => {
@@ -31,8 +32,8 @@ module.exports = (db) => {
     JOIN answers ON questions.id = question_id
     WHERE questions.id BETWEEN $1 AND $2` , [firstQuestion, lastQuestion])
               .then(data => {
-                console.log(data.rows);
                 const results = data.rows;  //
+                console.log(data.rows);
                 const templateVars = { results };//
                 res.render('take.ejs', templateVars); //
                 //res.json(data.rows[0]);
@@ -54,40 +55,64 @@ module.exports = (db) => {
 
     // on submit insert the data to the database "users_quizzes"
 
-    const quiz_id = req.params.id;
+    //const quiz_id = req.params.id;
     const user_id = req.session.id;
     console.log(req.body);
 
+    const usersAnswers = [
+      req.body.q1option,
+      req.body.q2option,
+      req.body.q3option,
+      req.body.q4option,
+      req.body.q5option
+    ];
+    console.log(usersAnswers);
+
     // `INSERT INTO users_quizzes (quiz_id, user_id, correct)
     // VALUES ($1,$2,$3)`, [quiz_id,user_id,];
-    const correct = function(quiz_id) {
+    const correct = function() {
       //array of users answers
       //get database answers
       // look at the true and false in the answers table
       // for each correct integer++
-      const usersAnswers = [
-        req.body.q1option,
-        req.body.q2option,
-        req.body.q3option,
-        req.body.q4option,
-        req.body.q5option
-      ];
+
 
       // need a function that compares users chosen answers with db answers
       // return a single value, the number of correct answers they have
       // this function returns the value for users_quizzes.correct
+    };
 
       db.query(`SELECT answers.text FROM answers
       JOIN questions ON questions.id = question_id
       JOIN quizzes ON quizzes.id = quiz_id
       WHERE correct = true
-      AND quiz_id = $1;`, [quiz_id]);
+      AND quiz_id = $1;`, [quiz_id])
+      .then(data => {
+        const results = data.rows;  //
+        console.log(results);
+
+        const correctAnswers = [
+          results[0].text,
+          results[1].text,
+          results[2].text,
+          results[3].text,
+          results[4].text,
+        ]
+        const score = getScore(usersAnswers, correctAnswers)
+        console.log(score);
+
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
       // this query returns ALL the correct answers for a given quiz AS A COLUMN
       //> how do i make this into an array
 
-      const integer = 0;
-      return integer;
-    };
+      //const integer = 0;
+      //return integer;
+
   });
 
 
@@ -98,5 +123,15 @@ module.exports = (db) => {
 };
 
 
+const getScore = function (array1, array2) {
+  let score = 0;
 
+  for (let i = 0; i < 6; i++) {
+    if (array1[i] === array2[i]) {
+      score += 1
+    }
+  }
+  return score;
+
+}
 
