5cf43abe4e13731b74f5244bc13c36785eb7b102
Aisha Sabrie <sabriealt@gmail.com>
2022-01-14 09:40:32 -0500

    fix seeding error

--- db/seeds/02_quizzes.sql
+++ db/seeds/02_quizzes.sql
@@ -1,6 +1,6 @@
 -- Widgets table seeds here (Example)
 INSERT INTO quizzes (user_id, name, created_at,listed)
-VALUES (2,'How well do you know sports?', now()',true),
+VALUES (2,'How well do you know sports?', now(),true),
 (4,'Do you actually remember BEDMAS?', now(),true),
 (3,'Finish the Vine qoutes', now(), true),
 (1,'World Capitals', now(),true),
